9871224d23360a0b6d78dc34191f2d58037edbd7
rmmcosta <ricardocosta101085@gmail.com>
2019-12-28 14:00:15 +0000

    finished local signin and signup

--- LocalSigninSignup/app/routes.js
+++ LocalSigninSignup/app/routes.js
@@ -1,42 +1,60 @@
-module.exports = (app,passport)=>{
-    app.get('/',(req,res)=>{
-        res.render('index.ejs');
+module.exports = (app, passport) => {
+
+    app.get('/', (req, res) => {
+        res.render('pages/index.ejs');
     });
 
     //login
-    app.get('/login',(req,res)=>{
-        res.render('login.ejs',{message:req.flash('loginMessage')});
+    app.get('/login', (req, res) => {
+        res.render('pages/login.ejs', { message: req.flash('loginMessage') });
     });
 
-    app.post('/login',(req,res)=>{
-        //passport stuff
-    });
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect: '/profile', // redirect to the secure profile section
+        failureRedirect: '/login', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages
+    }));
 
     //signup
-    app.get('/signup',(req,res)=>{
-        res.render('signup.ejs',{message:req.flash('signupMessage')});
+    app.get('/signup', (req, res) => {
+        res.render('pages/signup.ejs', { message: req.flash('signupMessage') });
     });
 
-    app.post('/signup',(req,res)=>{
-        //passport stuff
-    });
+    //process the signup form
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/profile', // redirect to the secure profile section
+        failureRedirect: '/signup', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages
+    }));
+
+    // app.post('/login', function (req, res, next) {
+    //     passport.authenticate('local-login', function (err, user, info) {
+    //         console.log('authenticate:', info);
+    //         if (err) { console.log(err);return next(err); }
+    //         if (!user) { return res.redirect('/login'); }
+    //         req.logIn(user, function (err) {
+    //             if (err) { return next(err); }
+    //             return res.redirect('/profile');
+    //         });
+    //     })(req, res, next);
+    // });
 
     //profile
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
-    app.get('/profile',isLoggedIn,(req,res)=>{
-        res.render('profile.ejs',{user:req.user});// get the user out of session and pass to template
+    app.get('/profile', isLoggedIn, (req, res) => {
+        res.render('pages/profile.ejs', { user: req.user });// get the user out of session and pass to template
     });
 
     //logout
-    app.get('/logout',(req,res)=>{
+    app.get('/logout', (req, res) => {
         req.logout();
         res.redirect('/');
     });
 
     // route middleware to make sure a user is logged in
-    function isLoggedIn(req,res,next) {
-        if(req.isAuthenticated()){
+    function isLoggedIn(req, res, next) {
+        if (req.isAuthenticated()) {
             return next();
         }
         res.redirect('/');
--- LocalSigninSignup/index.js
+++ LocalSigninSignup/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3030;
+const port = 8080;
 const mongoose = require('mongoose');
 const passport = require('passport');
 const morgan = require('morgan');
@@ -11,27 +11,41 @@ const session = require('express-session');
 const configDB = require('./config/database');
 
 //database
-mongoose.connect(configDB.url, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(configDB.url, { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log('we are connected!');
+});
+
 
 //uses
 app.use(morgan('dev')); // log 4xx and 5xx responses to console
-app.use(cookieParser); // read cookies (needed for auth)
-app.use(bodyParser); // get information from html forms
+app.use(cookieParser()); // read cookies (needed for auth)
+// get information from html forms
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 //view engine
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 //configure passport
 require('./config/passport')(passport);
-app.use(session({secret:'thesecretpassport', resave: true, saveUninitialized: true}));
+app.use(session({ secret: 'thesecretpassport', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
+//public folder
+app.use(express.static(__dirname + '/public'));
+
 //routes
-require('./app/routes')(app,passport);
+require('./app/routes')(app, passport);
 
 //launch
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Listen on localhost:${port}`);
 });
